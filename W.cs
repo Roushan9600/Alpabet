049af87b57bf8d1dac14abc30b2d84d0a2384203
Roushan9600 <87384224+Roushan9600@users.noreply.github.com>
2021-07-17 18:46:15 +0530

    W_patern

---
+++ W.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=5;i++)
+            {
+                for(int j=1;j<=5;j++)
+                {
+                    if(i<=5 && j==1 || i<=5 && j==5 || i==3 && j==3 ||i==4 &&j==2 || i==4 && j==4 )
+                    {
+                    Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ") ;
+                    }
+                }
+                
+                Console.WriteLine();
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
