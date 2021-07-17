91b0bf44984407c08fbedd504eb75a99c173f398
Roushan9600 <87384224+Roushan9600@users.noreply.github.com>
2021-07-17 18:41:53 +0530

    U_patern

---
+++ U.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=5;i++)
+            {
+                for(int j=1;j<=5;j++)
+                {
+                    if(i<=5&&j==1||i<=5&&j==4||i==5&&j<=3 )
+                    {
+                    Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ") ;
+                    }
+                }
+                
+                Console.WriteLine();
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
